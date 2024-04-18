54f20d559de329dca02a9ca4ae2aae6c8f2629ab
ecorbisiersimplon <ecorbisier.simplon@gmail.com>
2024-04-18 11:53:04 +0200

    Eric/2024_04_18-11:53:04/Reset total

--- database/sql/03_DML_referenciel.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO
-    `tpa_roles` (`role_code`, `role_name`)
-VALUES
-    ('role@webmaster', 'webmaster'),
-    ('role@admin', 'admin'),
-    ('role@user', 'utilisateur'),
-    ('role@registered', 'inscrit');
