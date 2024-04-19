6a2e0b646503c46aef86e4329c6c9f03b5f12aa1
ecorbisiersimplon <ecorbisier.simplon@gmail.com>
2024-04-20 00:39:46 +0200

    Eric/de/Ajout

--- serveur-backend/src/Controller/ApiUsersController.php
+++ serveur-backend/src/Controller/ApiUsersController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\TpaUsers;
 use App\Form\TpaUsersType;
 use App\Repository\TpaUsersRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -20,39 +19,27 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ApiUsersController extends AbstractController
 {
     #[Route('/', name: 'app_api_users_index', methods: ['GET'])]
-    public function index(TpaUsersRepository $tpaUsersRepository, SerializerInterface $serializer): JsonResponse
+    public function index(TpaUsersRepository $tpaUsersRepository): JsonResponse
     {
-        $usersList = $tpaUsersRepository->findAll();
-
-        $jsonUsersList = $serializer->serialize($usersList, 'json', ['groups' => 'users.show']);
-        return new JsonResponse($jsonUsersList, Response::HTTP_OK, [], true);
+        return $this->json($tpaUsersRepository->findAll(), 200, [], [
+            'groups' => ['users.index']
+        ]);
+        // return $this->render('api_users/index.html.twig', [
+        //     'tpa_users' => $tpaUsersRepository->findAll(),
+        // ]);
     }
 
-    /**
-     * Cette fonction PHP crée un nouvel objet TpaUsers en désérialisant les données JSON d'une requête
-     * POST.
-     * 
-     * @param Request request Le paramètre `Request` dans la méthode `new` représente la requête HTTP
-     * entrante. Il contient toutes les informations sur la requête, telles que les en-têtes, les
-     * paramètres et le contenu.
-     * @param SerializerInterface serializer Le paramètre `` dans l'extrait de code fait
-     * référence à une instance d'une classe qui implémente SerializerInterface de Symfony. Cette
-     * interface fournit des méthodes de sérialisation et de désérialisation des données, vous
-     * permettant de convertir des objets dans différents formats tels que JSON, XML ou YAML.
-     */
-    #[Route('/' | '', name: 'app_api_users_new', methods: ['POST'])]
+    #[Route(['', '/'], name: 'app_api_users_new', methods: ['POST'])]
     public function new(
         Request $request,
         #[MapRequestPayload(
-            serializationContext: [
-                'groups' => ['users.create']
-            ]
+            serializationContext: ['users.create']
         )]
         TpaUsers $user,
         EntityManagerInterface $em
     ) {
-
         $user->setUserCreateAt(new \DateTimeImmutable());
+        $user->setUserPassword("4566");
         $em->persist($user);
         $em->flush();
         return $this->json($user, 200, [], [
@@ -60,16 +47,18 @@ class ApiUsersController extends AbstractController
         ]);
     }
 
-
     #[Route('/{id}', name: 'app_api_users_show', methods: ['GET'])]
-    public function show(TpaUsers $tpaUser): Response
+    public function show(TpaUsers $tpaUser): JsonResponse
     {
-        return $this->render('api_users/show.html.twig', [
-            'tpa_user' => $tpaUser,
+        return $this->json($tpaUser, 201, [], [
+            'groups' => ['users.index', 'users.show']
         ]);
+        // return $this->render('api_users/show.html.twig', [
+        //     'tpa_user' => $tpaUser,
+        // ]);
     }
 
-    #[Route('/{id}', name: 'app_api_users_edit', methods: ['PUT'])]
+    #[Route('/{id}/edit', name: 'app_api_users_edit', methods: ['POST'])]
     public function edit(Request $request, TpaUsers $tpaUser, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(TpaUsersType::class, $tpaUser);
@@ -87,7 +76,7 @@ class ApiUsersController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_api_users_delete', methods: ['DELETE'])]
+    #[Route('/delete/{id}', name: 'app_api_users_delete', methods: ['POST'])]
     public function delete(Request $request, TpaUsers $tpaUser, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete' . $tpaUser->getId(), $request->getPayload()->get('_token'))) {
